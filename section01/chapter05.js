82a1b255af6166e1a5bf13fa55427b015385e957
Clt689 <kx1302@naver.com>
2025-01-14 16:11:36 +0900

    feat: 1.5) 자료형

---
+++ section01/chapter05.js
@@ -0,0 +1,26 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInt = -Infinity;
+
+let nan = NaN; // 수치 연산이 실패했을 때 결과값으로 보통 사용
+// console.log(1 * "hello"); // NaN 출력
+
+// 2. String Type
+let myName = "이정환";
+let introduceText = `${myName}`; // 동적으로 집어 넣을 수 있음
+// console.log(introduceText); // 템플릿 리터럴 문법
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (= 아무 것도 없다)
+let empty = null;
+
+// 5. Undefined Type
+let none;
+console.log(none);
