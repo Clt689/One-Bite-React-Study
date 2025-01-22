ae164e061c22e391c7c21df34fb3343ab475530b
Clt689 <kx1302@naver.com>
2025-01-22 15:03:19 +0900

    feat: 3.5) Node.js 라이브러리 사용하기

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,5 +1,10 @@
 import multiply, { add, sub } from "./math.js";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(multiply(2, 3));
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(multiply(2, 3));
