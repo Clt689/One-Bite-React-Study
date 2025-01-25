e8087e515c8c57d7fb0ec36511d2099b1ea25424
이한빈 <kx1302@naver.com>
2025-01-25 18:42:29 +0900

    feat: 4.3) React App 구동원리 살펴보기기

--- section02/chapter05.js
+++ section02/chapter05.js
@@ -1,42 +1,23 @@
-// 1. 배열 순회
-let arr = [1, 2, 3];
+function printAvgScore(students) {
+  for (let name in students) {
+    let sum = 0;
+    const { scores } = students[name];
 
-// 1.1 배열 인덱스
-for (let i = 0; i < arr.length; i++) {
-  // console.log(arr[i]);
+    for (let score of scores) {
+      sum += score;
+    }
+    const avg = sum / scores.length;
+    console.log(`${name}: ${avg}`);
+  }
 }
 
-let arr2 = [4, 5, 6, 7, 8];
-for (let i = 0; i < arr2.length; i++) {
-  // console.log(arr2[i]);
-}
+printAvgScore({
+  이정환: { hobby: "테니스", scores: [10, 20, 30, 40, 50] },
+  김효빈: { hobby: "테니스", scores: [90, 80, 30, 70, 50] },
+  홍길동: { hobby: "의적", scores: [100, 100, 20, 20, 50] },
+});
 
-// 1.2 for of 반복문 - 배열
-for (let item of arr) {
-  // console.log(item);
-}
-
-// 2. 객체 순회
-let person = {
-  name: "이한빈",
-  age: 27,
-  hobby: "풋살",
-};
-
-// 2.1 Object.keys 사용
-let keys = Object.keys(person);
-
-for (let key of keys) {
-  const value = person[key];
-  // console.log(key, value);
-}
-
-// 2.2 Object.values
-// = 객체에서 value 값들만 뽑아서 새로운 배열로 반환
-let values = Object.values(person);
-// console.log(values);
-
-// 2.3 for in - 객체체
-for (let key in arr2) {
-  console.log(key);
-}
+// 출력 결과 :
+// 이정환: 30
+// 김효빈: 64
+// 홍길동: 58
