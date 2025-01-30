d9685960c6dc16d1f5cc40fc51754b7f484e617b
이한빈 <kx1302@naver.com>
2025-01-30 16:04:33 +0900

    feat: 5.7) State와 Props

---
+++ section05/src/components/Bulb.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const Bulb = () => {
+  const [light, setLight] = useState("off");
+
+  console.log(light);
+  return (
+    <div>
+      {light === "on"
+        ? <h1 style={{ backgroundColor: "orange" }}>Bulb is On</h1>
+        : <h1 style={{ backgroundColor: "gray" }}>Off</h1>}
+      <button
+        onClick={() => {
+          setLight(light === "on" ? "off" : "on");
+        }}
+      >
+      {light === "on" ? "끄기" : "켜기"}
+      </button>
+    </div>
+  );
+}
+
+export default Bulb;
