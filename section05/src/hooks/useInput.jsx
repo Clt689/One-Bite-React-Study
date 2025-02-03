c9a69233af5de46e1205cfce7552979e34a437ab
Clt689 <kx1302@naver.com>
2025-02-03 15:03:38 +0900

    feat: 5.11) React Hooks

---
+++ section05/src/hooks/useInput.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function useInput() { // Custom Hook
+  const [input, setInput] = useState("");
+
+  const onChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  return [input, onChange];
+}
+
+export default useInput;
