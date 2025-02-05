75b3d8c0407c190f5ce7599de35ea5bc644ace5d
Clt689 <kx1302@naver.com>
2025-02-05 12:01:22 +0900

    feat: 8.2) UI 구현하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
+import Editor from './components/Editor'
+import Header from './components/Header'
+import List from './components/List'
 
 function App() {
 
   return (
     <>
+      <div className='App'>
+        <Header />
+        <Editor />
+        <List />
+      </div>
+      
     </>
   )
 }
