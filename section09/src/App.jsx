d32a44960ee71306294c5fc0907e6fac5b0b688c
Clt689 <kx1302@naver.com>
2025-02-07 15:04:52 +0900

    feat: 9.2) 투두리스트 업그레이드

--- section09/src/App.jsx
+++ section09/src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css'
-import { useState, useRef } from 'react'
+import { useRef, useReducer } from 'react'
 import Editor from './components/Editor'
 import Header from './components/Header'
 import List from './components/List'
-import Exam from './components/Exam'
 
 const mockData = [
   {
@@ -25,48 +24,64 @@ const mockData = [
     date: new Date().getTime(),
   },
 ]
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "create":
+      return [action.data, ...state];
+    case "update":
+      return state.map((item) =>
+        item.id === action.targetId
+          ? { ...item, isDone: !item.isDone }
+          : item
+      );
+    case "delete":
+      return state.filter((item) =>
+        item.id !== action.targetId
+      );
+    default:
+      return state;
+  }
   
+}
+
 function App() {
-  const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData)
   const idRef = useRef(3);
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime()
-    }
-
-    setTodos([newTodo, ...todos,])  
+    dispatch({
+      type: "create",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        data: new Date().getTime(),
+      }
+    }) 
   }
   
   const onUpdate = (targetId) => {
-    // todos State 값들 중에
-    // targetId와 일치하는 id를 갖는 투두 아이템의 isDone 변경
-
-    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소 데이터만 딱 바꾼 새로운 배열
-    setTodos(todos.map((todo) =>
-      todo.id === targetId
-        ? { ...todo, isDone: !todo.isDone }
-        : todo
-      )
-    )
+    dispatch({
+      type: "update",
+      targetId: targetId
+    })
   }
 
   const onDelete = (targetId) => {
-    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
-    setTodos(todos.filter((todo) => todo.id !== targetId))
+    dispatch({
+      type: "delete",
+      targetId: targetId
+    })
   }
 
   return (
     <>
-      <Exam />
-      {/* <div className='App'>
+      <div className='App'>
         <Header />
         <Editor onCreate={onCreate} />
         <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
-      </div> */}
+      </div>
     </>
   )
 }
