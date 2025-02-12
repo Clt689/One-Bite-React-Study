37143712c2d5c9f5ee3ea70d2a3b323f39ad1090
Clt689 <kx1302@naver.com>
2025-02-12 10:58:22 +0900

    feat: 8.4) Create - 투두 추가하기

--- section08 copy/src/App.jsx
+++ section08 copy/src/App.jsx
@@ -28,39 +28,27 @@ const mockData = [
 function App() {
   const [todos, setTodos] = useState(mockData);
   const idRef = useRef(3);
+
   const onCreate = (content) => {
     const newTodo = {
       id: idRef.current++,
       isDone: false,
       content: content,
-      date: new Date().getTime()
+      date: new Date().getTime
     }
-    setTodos([newTodo, ...todos,])  
+
+    setTodos([newTodo, ...todos]);
   }
-  
-  const onUpdate = (targetId) => {
-    // todos State 값들 중에
-    // targetId와 일치하는 id를 갖는 투두 아이템의 isDone 변경
-    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소 데이터만 딱 바꾼 새로운 배열
-    setTodos(todos.map((todo) =>
-      todo.id === targetId
-        ? { ...todo, isDone: !todo.isDone }
-        : todo
-      )
-    )
-  }
-  const onDelete = (targetId) => {
-    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
-    setTodos(todos.filter((todo) => todo.id !== targetId))
-  }
+
   return (
     <>
       <div className='App'>
         <Header />
         <Editor onCreate={onCreate} />
-        <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+        <List />
       </div>
     </>
   )
 }
+
 export default App
