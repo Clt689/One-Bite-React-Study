e24fcc8ee5b6741d98180395ef0393675dc4ee17
Clt689 <kx1302@naver.com>
2025-02-12 14:33:30 +0900

    feat: 8.7) Delete - 투두 삭제하기

--- section08 copy/src/components/List.jsx
+++ section08 copy/src/components/List.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./List.css";
 import TodoItem from "./TodoItem";
 
-const List = ({ todos, onUpdate }) => {
+const List = ({ todos, onUpdate, onDelete }) => {
   const [search, setSearch] = useState("");
 
   const onChangeSearch = (e) => {
@@ -29,10 +29,11 @@ const List = ({ todos, onUpdate }) => {
       <div className="todos_wrapper">
         {filteredTodos.map((todo) => {
           return (
-          <TodoItem
+            <TodoItem
               key={todo.id}
               {...todo}
               onUpdate={onUpdate}
+              onDelete={onDelete}
             />
           );
         })}
