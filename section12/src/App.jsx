1036621bcc4925bd7bbea62ad9a3a96dc1fccacd
Clt689 <kx1302@naver.com>
2025-02-19 17:16:01 +0900

    feat: day18 미션

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
 import Notfound from './pages/Notfound';
+import Edit from './pages/Edit';
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지(메인)
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -21,12 +22,14 @@ function App() {
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
         <Link to={"/diary"}>Diary</Link>
+        <Link to={"/edit"}>Edit</Link>
       </div>
       <button onClick={onClickButton}>New 페이지로 이동</button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
