30bf4b6616f6c3fd5cc806256014714e010a9f23
Clt689 <kx1302@naver.com>
2025-02-20 17:57:03 +0900

    12.7) 공통 컴포넌트 구현하기

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -5,6 +5,8 @@ import New from './pages/New';
 import Diary from './pages/Diary';
 import Notfound from './pages/Notfound';
 import { getEmotionImage } from './util/get-emotion-image';
+import Button from './components/Button';
+import Header from './components/Header';
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지(메인)
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -17,22 +19,36 @@ function App() {
   };
 
   return (
-    <>      
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
+    <>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} /> }
+        rightChild={<Button text={"Right"} />}
+      />
 
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-        <Link to={"/edit"}>Edit</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Button
+        text={"123"}
+        onClick={() => {
+          console.log("123번 버튼 클릭");
+        }}
+      />  
+  
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭");
+        }}
+      />  
+  
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭");
+        }}
+      />  
+  
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
