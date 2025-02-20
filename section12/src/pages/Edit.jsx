96bab1216ef2dc917eec402f08ac1ae169404b32
Clt689 <kx1302@naver.com>
2025-02-20 18:14:50 +0900

    12.8) 일기 관리 기능 구현하기 1

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -2,10 +2,8 @@ import { useParams } from "react-router-dom";
 
 const Edit = () => {
   const params = useParams();
-  console.log(params);
-
   return (
-    <div>{params.id}번 일기입니다</div>
+    <div>{params.id}번 일기 수정 페이지입니다.</div>
   );
  }
 
