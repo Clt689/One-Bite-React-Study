fdc7dd940473736bac7441b37a360a1f858b7919
Clt689 <kx1302@naver.com>
2025-02-21 11:46:38 +0900

    feat: onUpdate, onDelete 함수 추가

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -25,14 +25,26 @@ const mockData = [
 
 function reducer(state, action) {
   switch (action.type) {
-    case "CREATE": return [action.data, ...state];
-    // case "UPDATE": return;
-    // case "DELETE": return;
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      // item.id와 action.data.id가 같다면, 수정할 내용인 action.data를 넣기
+      return state.map((item) =>
+        String(item.id) === String(action.data.id)
+          ? action.data
+          : item
+      );
+    case "DELETE":
+      return state.filter(
+        (item) => String(item.id) !== String(action.id)
+      );
+    default:
+      return state;
   }
 }
 
 function App() {
-  const [data, dispatch] = useReducer(reducer, [mockData]);
+  const [data, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
   // 새로운 일기 추가
@@ -46,23 +58,50 @@ function App() {
         emotionId,
         content,
       }
-    })
-
+    });
   }
 
   // 기존 일기 수정
-  const onUpdate = () => {}
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content
+      }
+    })
+  }
 
   // 기존 일기 삭제
-  const onDelete = () => {}
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      id,
+    })
+  }
 
   return (
     <>
       <button onClick={() => {
         onCreate(new Date().getTime(), 1, "Hi");
       }}>
-        일기 추가 test
+        일기 추가 테스트
       </button>
+
+      <button onClick={() => {
+        onUpdate(1, new Date().getTime(), 3, "수정된 일기입니다.");
+      }}>
+        일기 수정 테스트
+      </button>
+
+      <button onClick={() => {
+        onDelete(1);
+      }}>
+        일기 삭제 테스트
+      </button>
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
