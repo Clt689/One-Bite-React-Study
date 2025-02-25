2483a55305cebe86b68365aa608355afe48695b4
Clt689 <kx1302@naver.com>
2025-02-25 11:36:30 +0900

    feat: New 페이지 UI 구현

---
+++ mission/day20/src/components/Editor.jsx
@@ -0,0 +1,61 @@
+import "./Editor.css";
+import Button from "./Button";
+import EmotionItem from "./EmotionItem";
+
+const emotionList = [
+  {
+    emotionId: 1,
+    emotionName: "완전 좋음"
+  },
+  {
+    emotionId: 2,
+    emotionName: "좋음"
+  },
+  {
+    emotionId: 3,
+    emotionName: "그럭저럭"
+  },
+  {
+    emotionId: 4,
+    emotionName: "나쁨"
+  },
+  {
+    emotionId: 5,
+    emotionName: "끔찍함"
+  },
+]
+
+const Editor = () => {
+  const emotionId = 1;
+
+  return (
+    <div className="Editor">
+      <section className="date_section">
+        <h4>오늘의 날짜</h4>
+        <input type="date" />
+      </section>
+      <section className="emotion_section">
+        <h4>오늘의 감정</h4>
+        <div className="emotion_list_wrapper">
+          {emotionList.map((item) => (
+            <EmotionItem
+              key={item.emotionId}
+              {...item}
+              isSelected={item.emotionId === emotionId}
+            />
+          ))}
+        </div>
+      </section>
+      <section className="content_section">
+        <h4>오늘의 일기</h4>
+        <textarea placeholder="오늘은 어땠나요? 😃"></textarea>
+      </section>
+      <section className="button_section">
+        <Button text={"취소하기"}/>
+        <Button text={"작성완료"} type={"POSITIVE"} />
+      </section>
+    </div>
+  );
+}
+
+export default Editor;
--- mission/day20/src/pages/New.jsx
+++ mission/day20/src/pages/New.jsx
@@ -1,6 +1,19 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+
 const New = () => {
   return (
-    <div>New</div>
+    <div>
+      <div>
+        <Header
+          title="새 일기 쓰기"
+          leftChild={<Button onClick text={"< 뒤로 가기"}/>}
+        />
+        <Editor />
+      </div>
+      <div></div>
+    </div>
   );
 }
 
