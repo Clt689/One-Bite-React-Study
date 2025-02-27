05ae0a43ade6250951015b5322855f02b50d5dc8
Clt689 <kx1302@naver.com>
2025-02-27 18:22:00 +0900

    feat: 12.16) Diary 페이지 구현하기

--- mission/day20/src/pages/Edit.jsx
+++ mission/day20/src/pages/Edit.jsx
@@ -4,28 +4,15 @@ import Button from "../components/Button";
 import Editor from "../components/Editor";
 import { useContext, useEffect, useState } from "react";
 import { DiaryStateContext, DiaryDispatchContext } from "../App";
+import useDiary from "../hooks/useDiary";
 
 const Edit = () => {
   const params = useParams();
   const nav = useNavigate();
   const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
-  const data = useContext(DiaryStateContext);
-  const [curDiaryItem, setCurDiaryItem] = useState();
 
-  // 일기의 기본 설정을 가져오기 위해, 찾는 일기의 id와 id가 같은 데이터만 가져오는 함수
-  useEffect(() => {
-    const currentDiaryItem = data.find(
-      (item) => String(item.id) === String(params.id)
-    )
-
-    if (!currentDiaryItem) { 
-      window.alert("존재하지 않는 일기입니다.");
-      nav("/", { replace: true });
-    }
-
-    setCurDiaryItem(currentDiaryItem);
-  }, [params.id])
-
+  const curDiaryItem = useDiary(params.id);
+     
   // 일기 수정 화면 - [삭제하기] 버튼 함수
   const onClickDelete = () => {
     if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
