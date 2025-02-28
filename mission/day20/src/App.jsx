43f44949572e85f6ff70ef9892e15485225667f7
Clt689 <kx1302@naver.com>
2025-02-28 14:28:17 +0900

    feat: 12.17) 웹 스토리지 이용하기

--- mission/day20/src/App.jsx
+++ mission/day20/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useReducer, useRef, createContext } from 'react';
+import { useReducer, useRef, createContext, useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import New from './pages/New';
@@ -7,54 +7,82 @@ import Diary from './pages/Diary';
 import Edit from './pages/Edit';
 import Notfound from './pages/Notfound';
 
-// 초기값 설정
-const mockData = [
-  {
-    id : 1,
-    createdDate: new Date("2025-02-24").getTime(),
-    emotionId : 1,
-    content: "1번 일기 내용",
-  },
-  {
-    id : 2,
-    createdDate: new Date("2025-02-22").getTime(),
-    emotionId : 5,
-    content: "2번 일기 내용",
-  },
-  {
-    id : 3,
-    createdDate: new Date("2025-01-14").getTime(),
-    emotionId : 3,
-    content: "3번 일기 내용",
-  },
-]
-
 function reducer(state, action) {
+  let nextState;
+
   switch (action.type) {
-    case "CREATE":
-      return [action.data, ...state];
-    case "UPDATE":
+    case "INIT": { 
+      return action.data;
+    }
+    case "CREATE": {
+      nextState = [action.data, ...state];
+      break;
+    }
+    case "UPDATE":{
       // item.id와 action.data.id가 같다면, 수정할 내용인 action.data를 넣기
-      return state.map((item) =>
+      nextState = state.map((item) =>
         String(item.id) === String(action.data.id)
           ? action.data
           : item
       );
-    case "DELETE":
-      return state.filter(
+      break;
+    }
+    case "DELETE": {
+      nextState = state.filter(
         (item) => String(item.id) !== String(action.id)
       );
+      break;
+    }
     default:
       return state;
   }
+
+  localStorage.setItem("diary", JSON.stringify(nextState));
+  return nextState;
 }
 
 export const DiaryStateContext = createContext();
 export const DiaryDispatchContext = createContext();
 
 function App() {
-  const [data, dispatch] = useReducer(reducer, mockData);
-  const idRef = useRef(3);
+  // 현재 로딩 중인지 아닌지의 상태를 보관하는 State가 필요
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+
+  useEffect(() => {
+    // LocalStorage에 diary라는 키값으로 저장된 데이터를 꺼내 옴
+    const storedData = localStorage.getItem("diary");
+    
+    if (!storedData) {
+      setIsLoading(false);
+      return ;
+    }
+    
+    // 문자열 형태로 저장되어있는 일기 데이터를, 배열 형태로 변환
+    const parsedData = JSON.parse(storedData);
+
+    if (!Array.isArray(parsedData)) { 
+      setIsLoading(false);
+      return;
+    }
+
+    let maxId = 0;
+    parsedData.forEach((item) => {
+      if (Number(item.id) > maxId) { 
+        maxId = Number(item.id)
+      }
+    })
+
+    idRef.current = maxId + 1;
+
+    dispatch({
+      type: "INIT",
+      data: parsedData,
+    });
+    setIsLoading(false);
+  }, [])
 
   // 새로운 일기 추가
   const onCreate = (createdDate, emotionId, content) => {
@@ -91,6 +119,10 @@ function App() {
     })
   }
 
+  if (isLoading) { 
+    return <div>데이터 로딩 중입니다..</div>
+  }
+
   return (
     <>
       <DiaryStateContext.Provider value={data}>
@@ -110,7 +142,6 @@ function App() {
           </Routes>
         </DiaryDispatchContext.Provider>
       </DiaryStateContext.Provider>
-      
     </>
   )
 }
