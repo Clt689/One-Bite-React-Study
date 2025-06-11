8b959696479ebc13cdd4ccf576f78ed2d43bbad3
이한빈 <kx1302@naver.com>
2025-06-11 22:15:10 +0900

    12.3) 페이지 라우팅 2. 라우팅 설정하기

--- section12_study/src/App.jsx
+++ section12_study/src/App.jsx
@@ -1,10 +1,23 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Diary from './pages/Diary'
+import Home from './pages/Home'
+import New from './pages/New'
+import Notfound from './pages/Notfound'
 
 function App() {
   
   return (
     <>
-      
+      <div>
+        Header
+      </div>
+      <Routes>
+        <Route path="/" element={ <Home />} />
+        <Route path="/new" element={ <New />} />
+        <Route path="/diary" element={ <Diary />} />
+        <Route path="*" element={ <Notfound />} />
+      </Routes>
     </>
   )
 }
